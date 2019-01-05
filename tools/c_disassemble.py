248059f0928cef9b037666c8195e00db4b604b9c
pspace <gitlab@interessierts.com>
2019-01-05 02:18:47 +0100

    Refined python disassemble script

--- tools/c_disassemble.py
+++ tools/c_disassemble.py
@@ -2,7 +2,6 @@ import r2pipe
 import argparse
 import base64
 
-
 def func_to_md_code(r2_pdfj):
     name = r2_pdfj["name"]
     ops = r2_pdfj["ops"]
@@ -14,19 +13,18 @@ def func_to_md_code(r2_pdfj):
         if "comment" in op.keys():
             comment = base64.b64decode(op["comment"])
 
-        line = op["opcode"]
+        line = op["disasm"]
         if  not op["opcode"] == op["disasm"]:
-            line = line + "\t\t;" + op["disasm"]
+            line = line + "\t\t;" + op["opcode"]
         line = line + "\t\t" + comment.decode("utf-8")
         lines.append(line)
-        #print(op["disasm"])
 
     lines.append("```")
 
     return lines
 
 
-def disas_file(path):
+def disas_file(path, fun_names):
     r2 = r2pipe.open(path)
     r2.cmd("aaa")
     functions = r2.cmdj("afljb")
@@ -34,6 +32,14 @@ def disas_file(path):
     for f in functions:
         name = f["name"]
         if ".imp." in name:
+            continue
+        
+        valid = False
+        for f in fun_names:
+            if f in name:
+                valid = True 
+                continue
+        if not valid:
             continue
 
         r2.cmd("s " + name)
@@ -44,12 +50,24 @@ def disas_file(path):
         with open(output_path, 'w') as out:
             out.write("\n".join(fs))
 
+def get_fun_names(paths):
+    names = []
+    for p in paths:
+        with open(p,'r') as f:
+            names.extend([l.strip() for l in f])
+    return names
 
 if __name__ == "__main__":
     # execute only if run as a script
     parser = argparse.ArgumentParser(description='Disassemble all functions with radare2')
     parser.add_argument('binary', metavar='BINARY', type=str, nargs=1,
-                        help='an integer for the accumulator')
+                        help='path to input binary')
+    parser.add_argument('functions', metavar='FUNCTIONNAMES', type=str, nargs=1,
+                        help='functions to disassemble')
+
 
     args = parser.parse_args()
-    disas_file(args.binary[0])
+    fun_names = get_fun_names(args.functions)
+    disas_file(args.binary[0], fun_names)
+
+
